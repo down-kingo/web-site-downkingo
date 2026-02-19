d0fd58c47d8a908e5ccbbbc57860a267eaf86bcd
Emanuel Nunes <emanuelsoares02@gmail.com>
2026-02-19 12:59:35 -0300

    fix: trailing slash, redirects, and clean canonical urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
     // This dramatically improves LCP on mobile devices
     inlineStylesheets: "always",
   },
+  trailingSlash: "never",
   integrations: [
     icon(),
     starlight({
