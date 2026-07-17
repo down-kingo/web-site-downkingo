c709bf0632dff18bae0addc1e09f0c5c4dc43650
Emanuel Nunes <emanuelsoares02@gmail.com>
2026-07-16 21:02:56 -0300

    fix(site): update current release to v3.1.2

---
+++ src/config/release.ts
@@ -0,0 +1,6 @@
+export const CURRENT_APP_VERSION = "3.1.2";
+export const CURRENT_APP_TAG = `v${CURRENT_APP_VERSION}`;
+
+// GitHub redirects this URL to the asset from the newest stable release.
+export const WINDOWS_LATEST_DOWNLOAD_URL =
+  "https://github.com/down-kingo/downkingo/releases/latest/download/DownKingo.exe";
